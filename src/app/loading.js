a6bc927c0143fd84ab8a5838edde0753852ae789
Shajib Chanda <sajibchanda73@gmail.com>
2026-05-17 15:56:54 +0600

    add loading and error

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Atom } from 'react-loading-indicators';
+
+const Loading = () => {
+    return (
+        <div className='flex justify-center'>
+            <Atom color="#29dbff" size="large" text="" textColor="" />
+        </div>
+    );
+};
+
+export default Loading;
